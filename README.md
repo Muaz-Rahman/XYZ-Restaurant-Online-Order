05523cf849bc07e3bf639d897e1c39a860b9b23b
Muaz <mrfarazi98@gmail.com>
2022-11-27 22:26:34 +0600

    Food menu backend completed. checkout.php on progress

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # XYZ-Restaurant-Online-Ordering-System
 
 An online food ordering system for an imaginary restaurant named XYZ. The website displays the user menu list of the day, a contact form for feedbacks and most importantly, the page that allows them to add foods to their carts and then order them once finished.
+
+The database files containing pre-existing dummy data are located in the Database Files for quick setup without the hassle of creating new database. For XAMPP, the folder inside Database Files needs to be put inside the XAMPP/mysql/data folder.
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '24a4069deda98eeb23244a1d07cddb8b7988af0c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '24a4069deda98eeb23244a1d07cddb8b7988af0c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
