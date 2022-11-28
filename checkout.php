1cf94664890f14fd8121b46efc6ada550841d25c
Muaz <mrfarazi98@gmail.com>
2022-11-28 19:03:07 +0600

    Menu to checkout page data flow fixed. Changed restaurant icon

--- checkout.php
+++ checkout.php
@@ -1,4 +1,16 @@
 <?php
+$arr = $_POST["arr"] ?? null;
+//if (!isset($arr)) {
+//    header("Location:menu.php");
+//}
+
+$con = new mysqli("localhost","root","","xyz_order_db");
+if($con->connect_error) die("Connection to database has failed");
+
+$item_name =  $item_price = "";
+
+
+//$sql_writer->store_result();
 session_start();
 ?>
 
@@ -16,10 +28,7 @@ session_start();
 
 <body style="background: var(--bs-border-color);">
     <nav class="navbar navbar-light navbar-expand-md sticky-top py-3" id="mainNav" style="background: var(--bs-gray-500);position: sticky;">
-        <div class="container-fluid"><a class="navbar-brand d-flex align-items-center" href="#"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-bezier">
-                        <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
-                        <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
-                    </svg></span><span>XYZ Food Orders</span></a>
+        <div class="container-fluid"><a class="navbar-brand d-flex align-items-center" href="#"><span style="margin-right: 5%"><img src="assets/img/logo.png" width="40px" height="60px"></span><span>XYZ Food Orders</span></a>
             <div class="collapse navbar-collapse" id="navcol-2">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"></li>
@@ -43,25 +52,46 @@ session_start();
         </div>
     </section>
     <h1 class="text-center" style="margin-bottom: 50px;"><span style="color: rgb(8, 153, 84);">Please Confirm Your Order</span></h1>
+
     <form>
         <div class="container" style="margin-top: 11px;margin-bottom: 27px;">
-            <div class="row">
-                <div class="col">
-                    <h1>Pizza</h1><label class="form-label" style="font-size: 20px;">Quantity</label><input class="form-control" type="text" value="1" style="width: 30%;">
-                    <p style="margin-top: 30px;">BDT 200</p>
+            <?php
+            foreach ($arr as $value) {
+                $sql_writer = $con->prepare("SELECT item_name,item_price FROM menu_table WHERE item_name='$value'");
+                if (!$sql_writer) echo "failed";
+                $sql_writer->execute();
+                $sql_writer->bind_result($item_name, $item_price);
+                $sql_writer->fetch();
+                $sql_writer->close();
+                echo "
+            <div class='row'>
+                <div class='col'>
+                    <h1>$item_name</h1><label class='form-label' style='font-size: 20px;'>Quantity:</label><input class='form-control' type='text' value='1' style='width: 30%;'>
+                    <p style='margin-top: 30px;'>BDT $item_price</p>
                 </div>
             </div>
+            ";
+            }
+            ?>
         </div>
+
+        <div class="container" style="margin-bottom: 3%">
+            <label class='form-label' style='font-size: 20px;'>Your Address:</label><textarea class="form-control" style="resize: none; height: 150px" > </textarea>
+        </div>
+
         <div class="container" style="margin-bottom: 5%;">
             <h1>Total: 1000 BDT</h1>
+            <button class="btn btn-primary" type="button" style="margin-top: 3%" onclick="">Refresh</button>
         </div>
         <div class="container" style="margin-bottom: 35px;margin-top: 4%;">
             <div class="row text-center">
                 <div class="col"><button class="btn btn-primary" type="button" style="background: var(--bs-green);">Place Order</button></div>
-                <div class="col"><button class="btn btn-primary" type="button" style="background: var(--bs-red);">Go Back</button></div>
+                <div class="col"><a href="menu.php" style="font-size: 1.2rem; color: red">Missed an item? Go Back</a></div>
             </div>
         </div>
     </form>
+
+
     <footer class="text-center bg-dark">
         <div class="container text-white py-4 py-lg-5" style="margin-top: -8px;margin-bottom: -8px;">
             <ul class="list-inline">
@@ -74,7 +104,8 @@ session_start();
                 <li class="list-inline-item"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-instagram text-light">
                         <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path>
                     </svg></li>
-            </ul><a href="#" style="color: var(--bs-navbar-active-color);">Contact Us</a>
+            </ul>
+            <a href="feedback_form.php" style="color: var(--bs-navbar-active-color);">Contact Us</a>
             <p class="text-muted mb-0">Copyright © 2022 XYZ Restaurant</p>
         </div>
     </footer>
@@ -83,11 +114,3 @@ session_start();
 
 </html>
 
-<!--$arr = $_POST["arr"] ?? null;-->
-<!--if (!isset($arr))-->
-<!--echo "Nothing has been chosen";-->
-<!--else-->
-<!--foreach ($arr as $value)-->
-<!--echo $value . " ";-->
-<!---->
-<!--?>-->
